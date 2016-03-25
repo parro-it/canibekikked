f702091ed5bf7a0e490029105d89c84b13e866c5
Andrea Parodi <andrea@parro.it>
2016-03-25 11:23:28 +0100

    CLI improved

--- cli.js
+++ cli.js
@@ -1,10 +1,43 @@
 'use strict';
-
+const ora = require('ora');
 const canibekiked = require('.');
+const spinner = ora('Retrieving packages');
+const logSymbols = require('log-symbols');
+
+spinner.start();
+
+function fail(err) {
+  process.stderr.write(`Unexpected error:\n${err.stack}\n`);
+}
+
+function trademarkedLog(p) {
+  return `${logSymbols.error} Package ${p.name} ` +
+    `has a trademarked name. ${JSON.stringify(p.trademarks)}\n`;
+}
+
 canibekiked()
   .then(results => {
-    results.on('error', console.error);
-    results.on('package-checked', console.log);
-    results.on('end', () => console.log('end'));
+    results.on('package-checking', name => {
+      spinner.color = 'yellow';
+      spinner.text = `Checking ${name}`;
+    });
+
+    results.on('error', fail);
+
+    results.on('package-checked', res => {
+      if (res.trademarks) {
+        spinner.color = 'red';
+        spinner.text = `Package ${res.name} has a trademarked name!`;
+      } else {
+        spinner.color = 'green';
+        spinner.text = `Package ${res.name} is not trademarked.`;
+      }
+    });
+
+    results.on('end', (passed, failed) => {
+      spinner.stop();
+      process.stdout.write(`${logSymbols.success} ${passed} packages has not trademarked names.\n`);
+      process.stdout.write(failed.map(trademarkedLog).join(''));
+    });
   })
-  .catch(console.error);
+  .catch(fail);
--- index.js
+++ index.js
@@ -7,12 +7,19 @@ const authorPackages = pify(require( 'npm-list-author-packages'));
 const isTrademarked = require('is-trademarked');
 const EventEmitter = require('events');
 
-const checkOnePackage = results => name =>
-  isTrademarked(name)
+const checkOnePackage = results => name => {
+  results.emit('package-checking', { name });
+  return isTrademarked(name)
     .then(trademarks => {
-      results.emit('package-checked', { name, trademarks});
+      if (trademarks) {
+        results.failed.push({ name, trademarks });
+      } else {
+        results.passed++;
+      }
+      results.emit('package-checked', { name, trademarks });
     })
     .catch(err => results.emit('error', err));
+};
 
 
 module.exports = co.wrap(function * canibekiked(user) {
@@ -20,9 +27,11 @@ module.exports = co.wrap(function * canibekiked(user) {
   const packages = yield authorPackages({ username });
 
   const results = new EventEmitter();
+  results.failed = [];
+  results.passed = 0;
   const checksCompleted = packages.map(checkOnePackage(results));
 
-  Promise.all(checksCompleted).then(() => results.emit('end'));
+  Promise.all(checksCompleted).then(() => results.emit('end', results.passed, results.failed));
 
   return results;
 });
